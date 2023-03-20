cfde10e10a53d74db0029046d6c2b517ba841cee
joshy56 <rossi_agustin_2003@hotmail.com>
2023-03-20 15:50:39 -0300

    Replace Player to PlayerAccount on Economy

--- src/main/java/io/github/joshy56/economicapi/economy/Economy.java
+++ src/main/java/io/github/joshy56/economicapi/economy/Economy.java
@@ -2,6 +2,7 @@ package io.github.joshy56.economicapi.economy;
 
 import io.github.joshy56.economicapi.attachable.MultiAttachable;
 import io.github.joshy56.economicapi.player.Player;
+import io.github.joshy56.economicapi.player.PlayerAccount;
 import io.github.joshy56.economicapi.response.Response;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,36 +14,52 @@ import java.util.Collection;
  */
 public interface Economy extends MultiAttachable {
     boolean has(@NotNull Player player, double amount);
+
     double balance(@NotNull Player player);
+
     @NotNull Response withdraw(@NotNull Player player, double amount);
+
     @NotNull Response deposit(@NotNull Player player, double amount);
+
     default @NotNull Collection<Bank> banks() {
         return attachments(Bank.class);
     }
-    default @NotNull Collection<Player> players() {
-        return attachments(Player.class);
+
+    default @NotNull Collection<PlayerAccount> players() {
+        return attachments(PlayerAccount.class);
     }
-    default @NotNull Response attachPlayer(@NotNull Player player) {
-        return attach(Player.class, player);
+
+    default @NotNull Response attachPlayer(@NotNull PlayerAccount player) {
+        return attach(PlayerAccount.class, player);
     }
-    default @NotNull Response detachPlayer(@NotNull Player player) {
-        return detach(Player.class, player);
+
+    default @NotNull Response detachPlayer(@NotNull PlayerAccount player) {
+        return detach(PlayerAccount.class, player);
     }
+
     default @NotNull Response detachAllPlayers() {
-        return detachAll(Player.class);
+        return detachAll(PlayerAccount.class);
     }
+
     default @NotNull Response attachBank(@NotNull Bank bank) {
         return attach(Bank.class, bank);
     }
+
     default @NotNull Response detachBank(@NotNull Bank bank) {
         return detach(Bank.class, bank);
     }
+
     default @NotNull Response detachAllBanks() {
         return detachAll(Bank.class);
     }
+
     @NotNull String name();
+
     int fractionalDigits();
+
     @NotNull String singularDisplayName();
+
     @NotNull String pluralDisplayName();
+
     @NotNull String format();
 }
